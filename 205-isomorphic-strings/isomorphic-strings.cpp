75f298affa98d5b05c9d8a5ddf9369913e8d460e
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-01 17:49:58 +0500

    Time: 1279 ms (6.77%) | Memory: 9.2 MB (94.17%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,27 +4,20 @@ public:
         if (s.size() != t.size()) {
             return false;
         }
-
         for (int i = 0; i < s.size(); i++) {
-            for (int j = i + 1; j < s.size(); j++) {
-
-                // if characters in s are same
-                if (s[i] == s[j]) {
-                    // then characters in t must also be same
+            for (int j = i + 1; j < s.size(); j++) {       
+                if (s[i] == s[j]) {              
                     if (t[i] != t[j]) {
                         return false;
                     }
                 }
-                // if characters in s are different
                 else {
-                    // then characters in t must also be different
                     if (t[i] == t[j]) {
                         return false;
                     }
                 }
             }
         }
-
         return true;
     }
 };
