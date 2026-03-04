719abeb3d758697655dd01a41c0aae61d1ae5eff
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-04 06:07:15 +0500

    Time: 3 ms (8.68%) | Memory: 30.4 MB (25.40%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,9 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-
         vector<int> temp(n);
-
         for(int i = 0; i < n; i++) {
             if(i < k)
                 temp[i] = nums[n - k + i];
