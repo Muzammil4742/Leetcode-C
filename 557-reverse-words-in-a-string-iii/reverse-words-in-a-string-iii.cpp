3ff79c1564900dcad9697da0a6d7873a6efbe584
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-05 23:49:17 +0500

    Time: 4 ms (37.12%) | Memory: 13.4 MB (41.03%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string result = "";
+        int start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+
+          
+            if (s[end] == ' ') {
+
+              
+                for (int i = end - 1; i >= start; i--) {
+                    result += s[i];
+                }
+
+                
+                result += ' ';
+
+                
+                start = end + 1;
+            }
+        }
+
+        
+        for (int i = s.length() - 1; i >= start; i--) {
+            result += s[i];
+        }
+
+        return result;
+    }
+};
