a11e15014a425b802875dfe34c78c80fed76e37d
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-09 17:50:48 +0500

    Time: 9 ms (44.59%) | Memory: 26.6 MB (55.25%) - LeetSync

--- 554-brick-wall/brick-wall.cpp
+++ 554-brick-wall/brick-wall.cpp
@@ -3,29 +3,26 @@ public:
     int leastBricks(vector<vector<int>>& wall) {
         
         unordered_map<long long,int> mp;
-        for(int i =0; i< wall.size();i++)
+        for(int i =0 ; i<wall.size() ; i++)
         {
-          vector<int>& row = wall[i];
-            long long sum=0;
-            for(int j=0 ; j<row.size() - 1;j++)
+            
+            long long freq=0;
+            vector<int>& row = wall[i];
+            for(int j=0 ; j< row.size() - 1 ; j++)
             {
-                sum= sum + row[j];
-                mp[sum]++;
+                freq = freq + row[j];
+                mp[freq]++;
             }
         }
-
-        int maxFreq = 0;
-
-     for(auto it = mp.begin(); it != mp.end(); it++)
-{
-    int freq = it->second;
-
-    if(freq > maxFreq)
-    {
-        maxFreq = freq;
-    }
-}
-
+        int maxFreq= 0;
+        for(auto it = mp.begin(); it!= mp.end() ; it++  )
+        {
+            int temp = it->second;
+            if(temp> maxFreq)
+            {
+                maxFreq = temp;
+            }
+        }
         return wall.size() - maxFreq;
     }
 };
