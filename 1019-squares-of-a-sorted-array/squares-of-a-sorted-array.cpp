1162d016899d4684bdffe4b19c7f78166572932c
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-14 23:37:10 +0500

    Time: 1099 ms (5.58%) | Memory: 30.1 MB (98.64%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -8,16 +8,29 @@ public:
             nums[i] = nums[i] * nums[i];
         }
 
-        
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = 0; j < n - i - 1; j++) {
-                if(nums[j] > nums[j + 1]) {
-                    swap(nums[j], nums[j + 1]);
+        // Step 2: two-end swapping logic
+        bool swapped;
+        do {
+            swapped = false;
+
+            // left to right
+            for(int k = 0; k < n - 1; k++) {
+                if(nums[k] > nums[k + 1]) {
+                    swap(nums[k], nums[k + 1]);
+                    swapped = true;
                 }
             }
-        }
 
-        // Step 3: return the sorted array
+            // right to left
+            for(int j = n - 1; j > 0; j--) {
+                if(nums[j] < nums[j - 1]) {
+                    swap(nums[j], nums[j - 1]);
+                    swapped = true;
+                }
+            }
+
+        } while(swapped);  // repeat until fully sorted
+
         return nums;
     }
 };
