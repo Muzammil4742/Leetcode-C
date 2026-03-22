45878c839040bbb6603bb2afdb4eff2d54b0dc79
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-22 22:39:53 +0500

    Time: 2 ms (70.21%) | Memory: 10 MB (44.37%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -3,30 +3,28 @@ public:
     bool checkInclusion(string s1, string s2) {
         int k = s1.size();
         int n = s2.size();
-
-        if (k > n) return false;
-
-        vector<int> freq1(26, 0), freq2(26, 0);
-
-        // Step 1: store frequency of s1 and first window of s2
-        for (int i = 0; i < k; i++) {
+        if(k>n)
+        {
+            return false;
+        }
+        vector <int> freq1(26,0), freq2(26,0);
+        for(int i = 0 ; i< k ; i++)
+        {
             freq1[s1[i] - 'a']++;
             freq2[s2[i] - 'a']++;
         }
-
-        // Step 2: check first window
-        if (freq1 == freq2) return true;
-
-        // Step 3: sliding window
+        if (freq1 == freq2)
+        {
+            return true;
+        }
+        
         for (int i = k; i < n; i++) {
-            // add new character
             freq2[s2[i] - 'a']++;
-
-            // remove old character
             freq2[s2[i - k] - 'a']--;
-
-            // compare
-            if (freq1 == freq2) return true;
+            if (freq1 == freq2)
+            {
+                return true;
+            } 
         }
 
         return false;
