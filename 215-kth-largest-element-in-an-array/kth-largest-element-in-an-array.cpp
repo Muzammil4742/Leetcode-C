5d9f53c3a139a42043ca69f0b7935c0b59da3fd6
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-04-04 22:36:22 +0500

    Time: 29 ms (61.30%) | Memory: 59.3 MB (86.99%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -2,32 +2,11 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         int n = nums.size();
-        
-        unordered_map<int,int> max_largest;
-        
-        // Step 1: store frequency
-        for(int i = 0; i < n; i++) {
-            max_largest[nums[i]]++;
+        sort(nums.begin(), nums.end());
+        if(k>n)
+        {
+            return -1;
         }
-
-        // Step 2: store unique elements
-        vector<int> unique;
-        for(auto &it : max_largest) {
-            unique.push_back(it.first);
-        }
-
-        // Step 3: sort in descending order
-        sort(unique.begin(), unique.end(), greater<int>());
-
-        // Step 4: find kth largest using frequency
-        int count = 0;
-        for(int num : unique) {
-            count += max_largest[num];
-            if(count >= k) {
-                return num;
-            }
-        }
-
-        return -1; // safety
+        return nums[n-k];
     }
 };
