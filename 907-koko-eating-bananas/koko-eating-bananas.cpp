dff13a2a091728a4545fd533c3af11d5458ff6ec
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-04-10 06:48:51 +0500

    Time: 18 ms (9.82%) | Memory: 23 MB (44.98%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-        
         int minSpeed = 1;
-        int maxSpeed = *max_element(piles.begin(), piles.end());
-        
+        sort(piles.begin(), piles.end());
+        int maxSpeed = piles[n-1];
         while (minSpeed <= maxSpeed) {
             int mid = (minSpeed + maxSpeed) / 2;
             
