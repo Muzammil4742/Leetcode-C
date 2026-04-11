33e0c693429554e2003f982c86b685a6026df672
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-04-11 06:14:35 +0500

    Time: 0 ms (100.00%) | Memory: 12.6 MB (39.35%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,61 +1,61 @@
-class Solution {
-public:
-    int findPeakElement(vector<int>& nums) 
-    {
-        long left=0,right=nums.size()-1;
-        while(left<right)
-        {
-            long mid=left+(right-left)/2;
-            if(nums[mid]<nums[mid+1])
-            left=mid+1;
-            else 
-            right=mid;
-        }
-        return left;
-    }
-};
-
-
-
-
-
-
 // class Solution {
 // public:
-//     int findPeakElement(vector<int>& nums) {
-//         int n =  nums.size();
-//         int index= 0;
-//         if(n==2)
+//     int findPeakElement(vector<int>& nums) 
+//     {
+//         long left=0,right=nums.size()-1;
+//         while(left<right)
 //         {
-//             if(nums[0]>nums[1])
-//             {
-//                 return 0;
-//             }
-//             else if(nums[1]>nums[0])
-//             {
-//                 return 1;
-//             }
+//             long mid=left+(right-left)/2;
+//             if(nums[mid]<nums[mid+1])
+//             left=mid+1;
+//             else 
+//             right=mid;
 //         }
-//         for(int i=1; i< n ; i++)
-//         {
-//             if(i == n-1 )
-//             {
-//                 if(nums[i]> nums[i-1])
-//                 {
-//                     index = i;
-//                     break;
-//                 }
-//             }
-//             else
-//             {
-//             if(nums[i]> nums[i-1] && nums[i]> nums[i+1])
-//             {
-//                 index = i;
-//                 break;
-//             }
-//             }
-            
-//         }
-//         return index;
+//         return left;
 //     }
 // };
+
+
+
+
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n =  nums.size();
+        int index= 0;
+        if(n==2)
+        {
+            if(nums[0]>nums[1])
+            {
+                return 0;
+            }
+            else if(nums[1]>nums[0])
+            {
+                return 1;
+            }
+        }
+        for(int i=1; i< n ; i++)
+        {
+            if(i == n-1 )
+            {
+                if(nums[i]> nums[i-1])
+                {
+                    index = i;
+                    break;
+                }
+            }
+            else
+            {
+            if(nums[i]> nums[i-1] && nums[i]> nums[i+1])
+            {
+                index = i;
+                break;
+            }
+            }
+            
+        }
+        return index;
+    }
+};
